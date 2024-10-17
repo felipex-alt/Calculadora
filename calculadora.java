e7c8c03937fdf8a18887a0a6d75bdf44a50b8a84
felipex-al <felipe2007defef@gmail.com>
2024-10-17 13:05:52 -0500

    primer commit

---
+++ calculadora.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class calculadora{
+
+    public static void main(String[] args) {
+        Scanner   scanner = new Scanner(System.in);
+        System.out.println("Bienvenido a la calculadora ");
+
+    }      
+}    
